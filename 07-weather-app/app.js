83c4d426daa85074554d32083fa1023373810105
Phanender Reddy Kasireddy <phanender100@gmail.com>
2018-02-27 13:15:01 +0530

    change location using modal

--- 07-weather-app/app.js
+++ 07-weather-app/app.js
@@ -8,6 +8,21 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', getWeather);
 
 
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+
+    // Get and display weather with new values
+    getWeather();
+
+    // Close modal
+    $('#locModal').modal('hide');
+});
+
+
 // Get weather
 function getWeather(){
     weather.getWeather()
